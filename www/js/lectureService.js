89c0b71638badc1e03ad58f739c24943869fafa7
LukaszB <lukasz.bocianski@gmail.com>
2015-04-07 18:21:49 +0200

    Added rating directive. Added lectudeDetail card Chenged display of lectureTab

--- www/js/lectureService.js
+++ www/js/lectureService.js
@@ -1,4 +1,4 @@
-angular.module('evapp.services').factory('Lectures', function() {
+angular.module('evapp.services').factory('Lectures', function(Speakers) {
     // Might use a resource here that returns a JSON array
 
     // Some fake testing data
@@ -14,10 +14,12 @@ angular.module('evapp.services').factory('Lectures', function() {
             {key: "Hybrid"},
             {key: "Busines"}
         ],
+        speaker: Speakers.get(1),
+        date: new Date('2015', '11' ,'13', '10','45'),
         icon: "ion-home"
     },{
         id: 1,
-        name: 'Future of Hybrid mobile apps',
+        name: 'Ionic - best hybrid framework',
         shortDescription: 'What you want to know about future of mobile apps',
         description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta",
         keywords: [
@@ -27,10 +29,12 @@ angular.module('evapp.services').factory('Lectures', function() {
             {key: "Hybrid"},
             {key: "Busines"}
         ],
+         speaker: Speakers.get(2),
+         date: new Date('2015-03-25T12:00:00'),
         icon: "ion-heart"
     },{
         id: 2,
-        name: 'Future of Hybrid mobile apps',
+        name: 'Cocos2d-x - games',
         shortDescription: 'What you want to know about future of mobile apps',
         description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta",
         keywords: [
@@ -40,6 +44,8 @@ angular.module('evapp.services').factory('Lectures', function() {
             {key: "Hybrid"},
             {key: "Busines"}
         ],
+        speaker: Speakers.get(3),
+         date: new Date('2015', '11' ,'13', '10','45'),
         icon: "ion-star"
     }];
 
--- www/js/scheduleDetailController.js
+++ www/js/scheduleDetailController.js
@@ -1,4 +1,37 @@
 angular.module('evapp.controllers')
-    .controller('ScheduleDetailCtrl', function ($scope, $stateParams, Lectures) {
-        $scope.lecture = Lectures.get($stateParams.scheduleId);
-    });
+
+.controller('ScheduleDetailCtrl', function ($scope, $ionicPopup, $timeout, $stateParams, Lectures) {
+    $scope.lecture = Lectures.get($stateParams.scheduleId);
+    $scope.rate = 3;
+    $scope.max = 5;
+    
+    $scope.showRatePopup = function() {
+  // An elaborate, custom popup
+  var myPopup = $ionicPopup.show({
+    template: '<rating ng-model="rate" max="max"></rating>',
+    title: 'Rate lecture:',
+    subTitle: $scope.lecture.name,
+    scope: $scope,
+    buttons: [
+      { text: 'Cancel' },
+      {
+        text: '<b>Rate</b>',
+        type: 'button-positive',
+        onTap: function(e) {
+          if (false) {
+            //don't allow the user to close unless he enters wifi password
+            e.preventDefault();
+          } else {
+          }
+        }
+      }
+    ]
+  });
+  myPopup.then(function(res) {
+    console.log('Tapped!', res);
+  });
+  $timeout(function() {
+     myPopup.close(); //close the popup after 10 seconds for some reason
+  }, 10000);
+ };
+});
